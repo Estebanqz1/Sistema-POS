c82a6112f442fed38557e8ab9f2e520edb585b65
EstebanQz <quiroz.esteban.e@gmail.com>
2023-08-03 04:51:04 -0300

    implementamos una clase genérica para la paginación de nuestras APIS para el listado de datos, posteriormente usaremos los diferentes patrones de diseño (Generic Repository y UnitOfWork).

---
+++ POS.Infrastructure/Commons/Bases/BasePaginationRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace POS.Infrastructure.Commons.Bases
+{
+    public class BasePaginationRequest
+    {
+        #region Propiedades
+
+        public int NumPage { get; set; } = 1;
+        public int NumRecordsPage { get; set; } = 10;
+        private readonly int NumMaxRecordsPage = 50;
+        public string Order { get; set; } = "asc";
+        public string? Sort { get; set; } = null;
+        public int Records
+        {
+            get => NumRecordsPage;
+            set
+            {
+                NumRecordsPage = (value > NumMaxRecordsPage) ? NumMaxRecordsPage : value;
+            }
+        }
+        #endregion
+
+        #region Constructror
+        #endregion
+
+        #region Metodos
+        #endregion
+    }
+}
---
+++ POS.Infrastructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //Declaracion de las interfaces a nivel de repository
+        ICategoryRepository Category { get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
